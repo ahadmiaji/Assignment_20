021298d5a281754d70e3d3548445a16291a13ca5
ahadmiaji <ahadmiaji@gmail.com>
2024-01-09 13:18:29 +0600

    Assignment_20

---
+++ app.js
@@ -0,0 +1,71 @@
+// Basic Configuration Lib Import
+const express = require('express');
+const router = require('./src/routes/api');
+const app = new express();
+const cookieParser = require('cookie-parser')
+
+// Security Variable Configuration
+const dotenv = require('dotenv');
+dotenv.config({path:'./.env'}); 
+
+// Security Middleware Lib Import
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cors = require('cors'); 
+const hpp = require('hpp'); 
+
+// Security Middleware Implement
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+// Body Parser & Cookie Implement
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Request Rate Limit
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 50,
+    message: "Too many requests from this IP, please try again later."
+})
+app.use(limiter);
+
+
+// Database Lib Import
+const mongoose = require('mongoose'); 
+
+const HOST = process.env.HOST;
+const DATABASE_NAME = process.env.DATABASE_NAME; 
+const DATABASE = `${process.env.DATABASE}/${DATABASE_NAME}`; 
+
+let URI = `mongodb://127.0.0.1:27017/Assignment_20`
+let OPTION = { user: "", pass: "", autoIndex: true }
+mongoose
+  .connect(URI, OPTION)
+  .then(() => console.log("Database is Connected ."))
+  .catch((err) => console.log(err))
+
+
+// Routing Implement
+app.use("/api/salse", router)
+
+// Undefined Route Implement
+app.use('*', (req,res)=>{
+    res.status(404).json({status: "Failed!", data: "404 Error! Wrong route!"});
+})
+
+// // Front-End Connection
+// const path = require('path');
+// app.use(express.static('Client/dist'));
+// app.get('*', function(req,res) {
+//   res.sendFile(path.resolve(__dirname,'Client','dist','index.html'))
+// })
+
+
+module.exports=app;
